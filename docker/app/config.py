e4b98c0df048c35306cf613f8621e37bf2853e55
Kota-Ohno <manntamann0206@gmail.com>
2024-05-23 13:54:42 +0900

    データセットの移行

--- docker/app/config.py
+++ docker/app/config.py
@@ -1,14 +1,14 @@
 import torch
 # ハイパーパラメータの設定
 MAX_SEQ_LENGTH = 512
-HIDDEN_SIZE = 512
+HIDDEN_SIZE = 256
 NUM_HEADS = 8
 NUM_LAYERS = 6
-D_FF = 2048  # フィードフォワードネットワークの内部層の次元
+D_FF = 1024  # フィードフォワードネットワークの内部層の次元
 DROPOUT_RATE = 0.1
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 NUM_EPOCHS = 10000  # トレーニングを実行するための設定
-PATIENCE = 5
+PATIENCE = 2
 WARMUP_STEPS = 4000
 BATCH_SIZE = 16
 LEARNING_RATE = 0.001
--- docker/app/text_tokenizer.py
+++ docker/app/text_tokenizer.py
@@ -49,8 +49,8 @@ def process_and_save_data(train_dataset, val_dataset, lang_src, lang_tgt, path_t
 
     # ボキャブラリの作成
     print("======= build vocabulary now ======")
-    base_vocabulary = build_vocabulary(tokenized_train_src)
-    destination_vocabulary = build_vocabulary(tokenized_train_tgt)
+    base_vocabulary = build_vocabulary(tokenized_train_src + tokenized_val_src)
+    destination_vocabulary = build_vocabulary(tokenized_train_tgt + tokenized_val_tgt)
     print("======= build vocabulary finished ======")
 
     # ボキャブラリの保存
@@ -60,11 +60,6 @@ def process_and_save_data(train_dataset, val_dataset, lang_src, lang_tgt, path_t
     vocab_output_path = os.path.join("models", "vocab_output.pth")
     torch.save(destination_vocabulary, vocab_output_path)
     print("======= save vocabulary finished ======")
-
-    print(len(tokenized_train_src))
-    print(len(tokenized_train_tgt))
-    print(len(tokenized_val_src))
-    print(len(tokenized_val_tgt))
 
     # トークンからインデックスへのマッピングを一度取得
     base_stoi = base_vocabulary.get_stoi()
@@ -80,41 +75,22 @@ def process_and_save_data(train_dataset, val_dataset, lang_src, lang_tgt, path_t
 
 def main():
 
-    # データとボキャブラリの読み込み
-    train_data = load_dataset("Amani27/massive_translation_dataset", split="train")
-    val_data = load_dataset("Amani27/massive_translation_dataset", split="validation")
-
-    train_dataset = set_data(train_data[TRANSLATION_SOURCE], train_data[TRANSLATION_DESTINATION])
-    val_dataset = set_data(val_data[TRANSLATION_SOURCE], val_data[TRANSLATION_DESTINATION])
-
-    # 必要
-    train_dataset = train_dataset[:]
-    val_dataset = val_dataset[:]
-
-    # ソースとターゲットをペアにする
-    paired_train_dataset = list(zip(train_dataset[0], train_dataset[1]))
-    paired_val_dataset = list(zip(val_dataset[0], val_dataset[1]))
-
-    # 2つ目のデータセットを読み込む
-    train_data2 = load_dataset("Verah/JParaCrawl-Filtered-English-Japanese-Parallel-Corpus", split="train")
+    # データセットを読み込む
+    train_data = load_dataset("Verah/JParaCrawl-Filtered-English-Japanese-Parallel-Corpus", split="train")
 
     # model2_acceptedカラムが1のデータのみを抽出
-    filtered_train_data2 = train_data2.filter(lambda filter: filter['model2_accepted'] == 1)
+    filtered_train_data = train_data.filter(lambda filter: filter['model2_accepted'] == 1)
 
     # データセットを設定
-    train_dataset2 = set_data(filtered_train_data2[TRANSLATION_SOURCE2], filtered_train_data2[TRANSLATION_DESTINATION2])
+    train_dataset = set_data(filtered_train_data[TRANSLATION_SOURCE2], filtered_train_data[TRANSLATION_DESTINATION2])
 
-    # train_data2を学習用データとテストデータに分割
-    train_dataset2, val_dataset2 = train_test_split(train_dataset2, test_size=0.1, random_state=42)
-
-    # データセットを合体
-    paired_train_dataset += train_dataset2
-    paired_val_dataset += val_dataset2
+    # train_dataを学習用データとテストデータに分割
+    train_dataset, val_dataset = train_test_split(train_dataset, test_size=0.1, random_state=42)
 
     # データのトークナイズと保存
     train_data_path = "tokenized_train_data.pth"
     val_data_path = "tokenized_val_data.pth"
-    train_token_ids, val_token_ids = process_and_save_data(paired_train_dataset, paired_val_dataset, TRANSLATION_SOURCE, TRANSLATION_DESTINATION, train_data_path, val_data_path)
+    train_token_ids, val_token_ids = process_and_save_data(train_dataset, val_dataset, TRANSLATION_SOURCE, TRANSLATION_DESTINATION, train_data_path, val_data_path)
     print("finished")
 
 if __name__ == "__main__":
